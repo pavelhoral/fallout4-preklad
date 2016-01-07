b16648ea6f6ce278dba61e4f7092e9f091ad91b8
pavelhoral <pavelhoral@localhost>
2016-01-07 10:16:03 +0100

    INNR rendering.

--- script/modfile.js
+++ script/modfile.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Clean-up translation files by removing any string which does not belong into its EDID range.
  */
@@ -20,13 +21,31 @@ function readStrings(modfile) {
     return new parseStrings.StringsReader().readByModfile(program.args[0], 'en');
 }
 
+function renderInnr(innr) {
+    var rowCount = Math.max.apply(null, innr.map(part => part.choices.length)),
+        rows = [];
+    innr.forEach((part) => {
+        for (let i = 0; i < part.choices.length; i++) {
+            rows[i] = (rows[i] || '') + (part.choices[i].name || '') + '\t"' +
+                    part.choices[i].conditions.join('\n') + '"\t';
+        }
+        for (let i = part.choices.length; i < rowCount; i++) {
+            rows[i] = (rows[i] || '') + '\t\t';
+        }
+    });
+    return rows.join('\n');
+}
+
 function extractInnrs(modfile) {
     var modfileSource = new parseSource.FileSource(modfile),
         modfileParser = new parseModfile.ModfileParser(modfileSource),
         innrExtractor = new modfileInnr.InnrExtractor(readStrings(modfile));
     modfileParser.parse(innrExtractor);
     modfileSource.close();
-    console.log(util.inspect(innrExtractor.innrs, false, null));
+    Object.keys(innrExtractor.innrs).forEach(key =>  {
+        console.log(key);
+        console.log(renderInnr(innrExtractor.innrs[key]));
+    });
 }
 
 extractInnrs(program.args[0]);
