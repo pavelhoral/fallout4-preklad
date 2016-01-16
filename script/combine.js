b41613905378f3f2af3e80bf021a3e48af0bac81
Pavel Horal <pavel.horal@orchitech.cz>
2016-01-16 11:40:31 +0100

    Combine script can do a full merge.

--- script/combine.js
+++ script/combine.js
@@ -3,16 +3,25 @@
  * Combine two or more SST XML files into one.
  */
 var fs = require('fs'),
+    path = require('path'),
     xml2js = require('xml2js'),
     program = require('commander');
 
 program.
     usage('[options] <file ...>').
+    option('-a, --all', 'Combine all translated files.').
     option('-o, --output [file]', 'Write combined result into a file.').
     parse(process.argv);
 
-if (program.args.length !== 2) {
+if (program.all && program.args.length || !program.all && program.args.length < 2) {
     program.help();
+}
+
+var files = program.args;
+if (program.all) {
+    files = fs.readdirSync(path.resolve(__dirname, '../translated')).map(filename => {
+        return path.resolve(__dirname, '../translated', filename);
+    });
 }
 
 function loadXml(xmlPath) {
@@ -26,7 +35,7 @@ function loadXml(xmlPath) {
 
 var combined = null;
 
-program.args.reduce((filePath) => {
+files.forEach((filePath) => {
     if (!combined) {
         combined = loadXml(filePath);
     } else {
