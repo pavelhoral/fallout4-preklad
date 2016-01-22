5b4eaff90c9bad04ccae66b792da87891898da0b
pavelhoral <pavelhoral@localhost>
2016-01-22 21:44:56 +0100

    Fixed NOINFO 00.

--- script/compare.js
+++ script/compare.js
@@ -8,7 +8,8 @@ var fs = require('fs'),
 
 program.
     usage('[options] <first-file> <second-file>').
-    option('-v, --verbose', 'Print out the differences.').
+    option('-d, --different', 'Print out the different translations.').
+    option('-m, --missing', 'Print out the missing translation.').
     parse(process.argv);
 
 if (program.args.length !== 2) {
@@ -24,9 +25,9 @@ function loadStrings(xmlPath) {
     });
     xmlObject.SSTXMLRessources.Content[0].String.forEach((string) => {
         var stringId = string.EDID[0] + ' ' + string.REC[0] + ' ' + string.Source[0];
-	if (stringTable[stringId]) {
-	    console.warn('Duplicate identifier:', stringId);
-	}
+    if (stringTable[stringId]) {
+        console.warn('Duplicate identifier:', stringId);
+    }
         stringTable[stringId] = string;
     });
     return stringTable;
@@ -37,18 +38,18 @@ var firstStrings = loadStrings(program.args[0]),
     diffStats = {
         same: 0,
         diff: 0,
-	missing: 0
+        missing: 0
     },
     stringDiffs = [];
 
 Object.keys(firstStrings).sort().forEach((stringId) => {
     if (!secondStrings[stringId]) {
         diffStats.missing++;
-	stringDiffs.push({
-	    id: stringId,
-	    source: firstStrings[stringId].Source[0],
-	    first: firstStrings[stringId].Dest[0]
-	});
+        stringDiffs.push({
+            id: stringId,
+            source: firstStrings[stringId].Source[0],
+            first: firstStrings[stringId].Dest[0]
+        });
     } else if (firstStrings[stringId].Dest[0] === secondStrings[stringId].Dest[0]) {
         diffStats.same++;
     } else {
@@ -64,13 +65,18 @@ Object.keys(firstStrings).sort().forEach((stringId) => {
 
 console.log('[COMPARE]', JSON.stringify(diffStats));
 
-if (program.verbose) {
-    stringDiffs.forEach((diff) => {
-        console.log('----');
+if (program.different) {
+    stringDiffs.filter(diff => diff.second).forEach(diff => {
+        console.log('----D');
         console.log(diff.id);
         console.log(diff.first);
-	if (diff.second) {
-            console.log(diff.second);
-	}
+        console.log(diff.second);
+    });
+}
+if (program.missing) {
+    stringDiffs.filter(diff => !diff.second).forEach((diff) => {
+        console.log('----M');
+        console.log(diff.id);
+        console.log(diff.first);
     });
 }
