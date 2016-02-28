9ff6dc3ca1f5d0f9e513ccf8b0a1b641d09156fc
Pavel Horal <pavel.horal@orchitech.cz>
2016-02-28 12:50:42 +0100

    NOINFO 34 translated.

---
+++ script/modfile/dial.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var parseModfile = require('../parse/parse-modfile');
+
+var MODFILE_TYPES = new parseModfile.ModfileType([
+    'QUST', 'DIAL', 'INFO', 'EDID'
+]);
+
+function renderFormId(formId) {
+    var hexId = formId.toString(16).toUpperCase();
+    return '[' + '00000000'.substring(0, 8 - hexId.length) + hexId + ']';
+}
+
+/**
+ * ModfileHandler implementation for extracting DIAL/INFO tree.
+ */
+class DialExtractor {
+
+    constructor() {
+        this.dials = {};
+        this.context = null;
+    }
+
+    handleGroup(type, label, parse) {
+        if (type === 0 && label === MODFILE_TYPES.QUST) {
+            this.context = {};
+            parse(this);
+            this.context = null;
+        } else if (this.context && this.context.branch) {
+            this.context.branch = false;
+            parse(this);
+            this.context = {};
+        } else if (this.context && !this.context.branch) {
+            throw new Error("Unexpected GRUP record.");
+        }
+    }
+
+    handleRecord(type, size, flags, formId, parse) {
+        if (!this.context) {
+            return;
+        }
+        // Reset context
+        this.context.editorId = null;
+        this.context.branch = false
+        // Parse fields for EDID
+        parse(this);
+        // Expect GRUP branch for DIAL and QUST
+        if (type === MODFILE_TYPES.DIAL || type === MODFILE_TYPES.QUST) {
+            this.context.branch = true;
+        }
+        // Handle DIAL
+        if (type === MODFILE_TYPES.DIAL) {
+            this.context.dialogId = renderFormId(formId);
+            this.dials[this.context.dialogId] = [];
+        }
+        // Handle INFO
+        if (type === MODFILE_TYPES.INFO) {
+            // flags & 0x40 -> pouze oznacuji oddil
+            this.dials[this.context.dialogId].push(this.context.editorId || renderFormId(formId));
+        }
+        this.context.editorId = null;
+    }
+
+    handleField(type, size, buffer, offset) {
+        if (type === MODFILE_TYPES.EDID) {
+            this.context.editorId = buffer.toString('ascii', offset, offset + size - 1);
+        }
+    }
+
+}
+module.exports.DialExtractor = DialExtractor;
---
+++ script/parse/parse-modfile.js
@@ -0,0 +1,203 @@
+"use strict";
+
+/**
+ * Modfile entry type constant pool.
+ * Contains type names mapped onto their UINT32LE equivalents and vice-versa.
+ */
+class ModfileType {
+
+    constructor(types) {
+        types.forEach((type) => {
+            var encoded = this.encode(type);
+            this[type] = encoded;
+            this[encoded] = type;
+        });
+        Object.freeze(this);
+    }
+
+    encode(type) {
+        return type.charCodeAt(0) +
+                (type.charCodeAt(1) << 8) +
+                (type.charCodeAt(2) << 16) +
+                (type.charCodeAt(3) << 24);
+    }
+
+    decode(type) {
+        return String.fromCharCode(
+                type & 0xff,
+                type >> 8 & 0xff,
+                type >> 16 & 0xff,
+                type >> 24 & 0xff);
+    }
+
+}
+module.exports.ModfileType = ModfileType;
+
+/**
+ * Create basic MODFILE_TYPE pool.
+ */
+var MODFILE_TYPES = new ModfileType([
+    'GRUP', 'EDID', 'OFST'
+]);
+module.exports.MODFILE_TYPES = MODFILE_TYPES;
+
+/**
+ * Define default modfile parsing handler.
+ * Serves both as base and example handler implementation.
+ */
+class ModfileHandler {
+
+    constructor() {
+        this.parsingStack = [{
+            label: 'ROOT',
+            children: []
+        }];
+    }
+
+    /**
+     * Handle group based entry.
+     */
+    handleGroup(type, label, parse) {
+        var head = this.parsingStack[this.parsingStack.length - 1];
+        // Push group on the stack
+        this.parsingStack.push({
+            label: label,
+            children: []
+        });
+        // Parse children entries
+        parse(this);
+        // Remove from stack and add to parent
+        head.children.push(this.parsingStack.pop());
+    }
+
+    /**
+     * Handle record based entry.
+     */
+    handleRecord(type, size, flags, formId, parse) {
+        var head = this.parsingStack[this.parsingStack.length - 1];
+        // Push record on the stack
+        this.parsingStack.push({
+            type: type,
+            size: size,
+            flags: flags,
+            formId: formId
+        });
+        // Parse children entries
+        parse(this);
+        // Remove from stack and add to parent
+        head.children.push(this.parsingStack.pop());
+    }
+
+    /**
+     * Handle field based entry.
+     */
+    handleField(type, size, buffer, offset) {
+        var head = this.parsingStack[this.parsingStack.length - 1];
+        // Add editor identifier
+        if (type == MODFILE_TYPES.EDID) {
+            head.editorId = buffer.toString('ascii', offset, offset + size - 1);
+        }
+    }
+
+}
+module.exports.ModfileHandler = ModfileHandler;
+
+/**
+ * Simple parser for Bethesda's ESM/ESP modfiles.
+ * Parser reads provided data source and processes entries via provided entry handler.
+ */
+class ModfileParser {
+
+    constructor(source) {
+        // Set source
+        this.source = source;
+        // Freeze ourselves
+        Object.freeze(this);
+    }
+
+    /**
+     * Start modfile parsing with the given handler.
+     */
+    parse(handler) {
+        while (this.parseNext(handler)) { /* no-op */ }
+    }
+
+    /**
+     * Parse next entry and return its size in bytes.
+     */
+    parseNext(handler, assert) {
+        var buffer = this.source.read(24),
+            type = buffer.length === 24 ? buffer.readUInt32LE(0) : null;
+        if (assert && !type) {
+            throw new Error("Unexpected end of source reached.");
+        } else if (type === MODFILE_TYPES.GRUP) {
+            return this.parseGroup(buffer, handler);
+        } else if (type !== null) {
+            return this.parseRecord(type, buffer, handler) + 24;
+        }
+        return 0; // EOF
+    }
+
+    /**
+     * Parse group based entry and return its size (including header).
+     */
+    parseGroup(buffer, handler) {
+        var size = buffer.readUInt32LE(4),
+            label = buffer.readUInt32LE(8),
+            type = buffer.readInt32LE(12),
+            skip = size - 24;
+        handler.handleGroup(type, label, (handler) => {
+            while (skip > 0) {
+                skip -= this.parseNext(handler, true);
+            }
+        });
+        this.source.skip(skip);
+        return size;
+    }
+
+    /**
+     * Parse record based entry and return size of its data.
+     */
+    parseRecord(type, buffer, handler) {
+        var size = buffer.readUInt32LE(4),
+            flags = buffer.readUInt32LE(8),
+            formId = buffer.readUInt32LE(12),
+            skip = size;
+        handler.handleRecord(type, size, flags, formId, (handler) => {
+            var buffer = this.source.read(size);
+            if (flags & 0x00040000) {
+                // TODO GZIP handling
+            }
+            this.parseFields(buffer, handler);
+            skip = 0;
+        });
+        this.source.skip(skip);
+        return size;
+    }
+
+    /**
+     * Parse field based entries.
+     */
+    parseFields(buffer, handler) {
+        var offset = 0;
+        while (offset < buffer.length) {
+            offset += this.parseField(buffer, offset, handler);
+        }
+        return buffer.length;
+    }
+
+    /**
+     * Parse field at the specified offset.
+     */
+    parseField(buffer, offset, handler) {
+        var type = buffer.readUInt32LE(offset),
+            size = buffer.readUInt16LE(offset + 4);
+        if (type === MODFILE_TYPES.OFST) {
+            return buffer.length - offset;
+        }
+        handler.handleField(type, size, buffer, offset + 6);
+        return size + 6;
+    }
+
+}
+module.exports.ModfileParser = ModfileParser;
