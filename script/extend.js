dff4b6cc8590029bf4a0d4f511314589a48d1c40
Pavel Horal <pavel.horal@orchitech.cz>
2017-02-16 18:37:15 +0100

    WIP

---
+++ script/extend.js
@@ -0,0 +1,37 @@
+/*
+
+TES4 HEADER:
+
+COPY type
+STATIC dataSize (fieldy jsou staticke)
+SET flags to 0x00000000
+SET formId to 0x00000000
+SET revision to 0x00000000
+SET version to 0x8300
+SET unknown to 0x0000
+
+*/
+
+/*
+
+RECORD HEADER:
+
+COPY type
+COUNT dataSize
+COPY flags
+COPY formId
+SET revision to 0x00000000
+SET version to 0x8300
+SET unknown to 0x0000
+...
+
+*/
+
+
+/*
+
+GRUP HEADER:
+
+
+
+*/
